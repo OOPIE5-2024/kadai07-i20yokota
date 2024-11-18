30db5f051010033e9516900799c6e466931a9acb
i20yokota <i20yokota@tokuyama.kosen-ac.jp>
2024-11-18 10:35:29 +0900

    クラス図とオブジェクト図に沿ってプログラムを作成しました

--- src/ex07/Ex7.java
+++ src/ex07/Ex7.java
@@ -9,13 +9,19 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 public class Ex7 {
 
   private JFrame frame;
-  private JPanel colorPanel;
+  private DiskPanel diskPanel;
   private CurrentColor crrntBckColor = new CurrentColor();
+  private CurrentColor crrntForColor = new CurrentColor();
 
+  final int LARGE_RADIUS = 250;
+  final int MEDIUM_RADIUS = 100;
+  final int SMALL_RADIUS = 25;
   /**
    * Launch the application.
    */
@@ -54,7 +60,7 @@ public class Ex7 {
     btnRed.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         Color c = crrntBckColor.changeRed();
-        colorPanel.setBackground(c);
+        diskPanel.setBackground(c);
       }
     });
     btnPanel.add(btnRed);
@@ -63,7 +69,7 @@ public class Ex7 {
     btnGreen.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         Color c = crrntBckColor.changeGreen();
-        colorPanel.setBackground(c);
+        diskPanel.setBackground(c);
       }
     });
     btnPanel.add(btnGreen);
@@ -72,14 +78,82 @@ public class Ex7 {
     btnBlue.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         Color c = crrntBckColor.changeBlue();
-        colorPanel.setBackground(c);
+        diskPanel.setBackground(c);
       }
     });
     btnPanel.add(btnBlue);
     
-    colorPanel = new JPanel();
-    colorPanel.setBackground(Color.BLACK);
-    frame.getContentPane().add(colorPanel, BorderLayout.CENTER);
+    diskPanel = new DiskPanel();
+    diskPanel.setBackground(Color.BLACK);
+    frame.getContentPane().add(diskPanel, BorderLayout.CENTER);
+    diskPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+    
+    
+    JPanel btnPanel2 = new JPanel();
+    frame.getContentPane().add(btnPanel2, BorderLayout.SOUTH);
+    btnPanel2.setLayout(new GridLayout(0, 1, 0, 0));
+    
+    
+    JPanel bckButtonPanel = new JPanel();
+    btnPanel2.add(bckButtonPanel);
+    
+    JButton btnFRed = new JButton("fRed");
+    btnFRed.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        Color c = crrntForColor.changeRed();
+        diskPanel.setForeground(c);
+      }
+    });
+    bckButtonPanel.add(btnFRed);
+    
+    JButton btnFGreen = new JButton("fGreen");
+    btnFGreen.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        Color c = crrntForColor.changeGreen();
+        diskPanel.setForeground(c);
+      }
+    });
+    bckButtonPanel.add(btnFGreen);
+    
+    JButton btnFBlue = new JButton("fBlue");
+    btnFBlue.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        Color c = crrntForColor.changeBlue();
+        diskPanel.setForeground(c);
+      }
+    });
+    bckButtonPanel.add(btnFBlue);
+    
+    
+    JPanel radButtonPanel = new JPanel();
+    btnPanel2.add(radButtonPanel);
+    
+    JButton btnLarge = new JButton("Large");
+    btnLarge.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        diskPanel.setRadius(LARGE_RADIUS);
+        diskPanel.repaint();
+      }
+    });
+    radButtonPanel.add(btnLarge);
+    
+    JButton btnMedium = new JButton("Medium");
+    btnMedium.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        diskPanel.setRadius(MEDIUM_RADIUS);
+        diskPanel.repaint();
+      }
+    });
+    radButtonPanel.add(btnMedium);
+    
+    JButton btnSmall = new JButton("Small");
+    btnSmall.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        diskPanel.setRadius(SMALL_RADIUS);
+        diskPanel.repaint();
+      }
+    });
+    radButtonPanel.add(btnSmall);
   }
 
 }
